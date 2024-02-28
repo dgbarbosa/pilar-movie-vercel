9eb56d5f2ee02ebd172bc42bd589b1997dae2e61
Daniel Barbosa <daniel.barbosa@dattos.com.br>
2024-02-28 04:08:51 -0300

    fix: getUpcomingMovie data param

--- src/services/movieService/movieService.ts
+++ src/services/movieService/movieService.ts
@@ -16,8 +16,8 @@ export function movieService() {
 
   function getUpcomingMovies(): Promise<MoviesResponse> {
     const currentDate = new Date()
-    const day = `${currentDate.getDate() + 1}`.padStart(2,0)
-    const month = `${currentDate.getMonth() + 1}`.padStart(2,0)
+    const day = `${currentDate.getDate() + 1}`.padStart(2,'0')
+    const month = `${currentDate.getMonth() + 1}`.padStart(2,'0')
     const year = currentDate.getFullYear()
     
     return axios.get<MoviesResponse>("/movie/discover", { language: "pt", "primary_release_date.gte": `${year}-${month}-${day}` });
